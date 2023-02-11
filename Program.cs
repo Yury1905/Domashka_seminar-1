475a52f47c8475c14605e748e2cc54089e743aa3
Yury <jokofan@rambler.ru>
2023-02-11 23:27:23 +0300

    сделал то, что смог

--- Program.cs
+++ Program.cs
@@ -24,11 +24,46 @@
 
 //Задача 4!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-Console.Write("Введите число 1: ");
-int number1 =int.Parse (Console.ReadLine()!) ;
-Console.Write("Введите число 2: ");
-int number2 =int.Parse (Console.ReadLine()!) ;
-Console.Write("Введите число 3: ");
-int number3 =int.Parse (Console.ReadLine()!) ;
+//Console.Write("Введите число 1: ");
+//int number1 =int.Parse (Console.ReadLine()!) ;
+//Console.Write("Введите число 2: ");
+//int number2 =int.Parse (Console.ReadLine()!) ;
+//Console.Write("Введите число 3: ");
+//int number3 =int.Parse (Console.ReadLine()!) ;
+//int max = number1;
+//if(number1 > max)
+// {
+ //   (max=number1);
+//}
+//
+//if ( number2 > max )
+//{
+ //   ( max = number2 );
+//}
+//if( number3 > max )
+//{
+ //   ( max = number3 );
+//}
+//Console.Write($"Максимальное число  {max}");
 
 
+// Задача 6!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+//Console.Write("Введите число 1: ");
+//int number1 =int.Parse (Console.ReadLine()!) ;
+//if (number1 % 2 == 0)
+ //{
+  //  Console.Write ("Да");
+ //}
+//else
+//{
+   // Console.Write ("нет");
+//}
+
+//Задача 8!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+//Console.Write("Введите число 1: ");
+//int number1 =int.Parse (Console.ReadLine()!) ;
+//int i= 0;
+//for (i; i <= number1; i+2);
+//Console.WriteLine ($"{i}");
