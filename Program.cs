381995234e0d3300e3b011fd662d088b81d8229f
Yury <jokofan@rambler.ru>
2023-02-11 21:48:54 +0300

    задача 1 готова

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// задача 2
+//Console.Write("Введите число 1: ");
+//int number1 =int.Parse (Console.ReadLine()!) ;
+//Console.Write("Введите число 2: ");
+//int number2 =int.Parse (Console.ReadLine()!) ;
+
+
+//if (number1>number2) 
+//{
+ //   int max = number1;
+ //  int min = number2;
+
+//Console.Write($"Максимальное число  {max}");
+//Console.Write($"Минимальное число  {min}");
+//}
+//else
+//{
+   //int max = number2;
+   // int min = number1; 
+//Console.Write($"Максимальное число  {max}");
+//Console.Write($"Минимальное число число  {min}");
+//}
+
+
+//Задача 4!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+Console.Write("Введите число 1: ");
+int number1 =int.Parse (Console.ReadLine()!) ;
+Console.Write("Введите число 2: ");
+int number2 =int.Parse (Console.ReadLine()!) ;
+Console.Write("Введите число 3: ");
+int number3 =int.Parse (Console.ReadLine()!) ;
+
+
